a7e1bdea30162e2ebbcf5edf9ad673c8bf05d800
hironori <janhampino@gmail.com>
2018-10-03 11:44:41 +0000

    finish login section

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,16 @@ class SessionsController < ApplicationController
     user = User.find_by(email:params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to user
     else
       flash.new[:danger] ='メールアドレスまたはパスワードが正しくありません'
     render 'new'
     end
+  end
+  
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
