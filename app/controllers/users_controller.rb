570e2b5d04c1142f215a799070c1677293b66844
hironori <janhampino@gmail.com>
2018-10-04 06:49:51 +0000

    finish user setting page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -19,9 +21,14 @@ class UsersController < ApplicationController
       end
   end
   
-  def destroy
-    log_out
-    redirect_to root_url
+  def edit
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "ユーザー情報を更新しました"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
   
   private
@@ -30,4 +37,18 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
   
+    #beforeアクション
+  
+    def logged_in_user
+      unless logged_in?
+      store_location
+      flash[:danger] = "ログインしてください"
+      redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
