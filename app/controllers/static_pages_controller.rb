2952af3c5d2b0199f88d193d989442d52c83e50c
Hironori <janhampino@gmail.com>
2018-10-07 09:00:35 +0000

    reforming top page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,20 @@
 class StaticPagesController < ApplicationController
   
   def home
-    if logged_in?
-      @post  = current_user.posts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
-    end
+#    if logged_in?
+      @users = User.all
+#      paginate(page: params[:page])
+#      @post  = current_user.posts.build
+#      @feed_items = current_user.feed.paginate(page: params[:page])
+#    end
   end
 
   def about
   end
 
   def contact
+  end
+  
+  def punch
   end
 end
