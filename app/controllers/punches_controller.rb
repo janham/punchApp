061c311f7c71018e9c38fd5302e0aaf13f4b9e23
Hironori <janhampino@gmail.com>
2018-10-08 12:05:14 +0000

    break in making punch section to remake user login

--- app/controllers/punches_controller.rb
+++ app/controllers/punches_controller.rb
@@ -7,11 +7,11 @@ class PunchesController < ApplicationController
   end
 
   def create
-#    @punch = Punch.new(punch_params)
-#    @user = User.find(punch_params[:user_id])
-#    if @punch.save
-#      flash[:success] = "打刻が完了しました"
-      
+    @punch = Punch.new(punch_params)
+    @user = User.find(punch_params[:user_id])
+    if @punch.save
+      flash[:success] = "打刻が完了しました"
+    end
   end
 
   def edit
@@ -19,4 +19,13 @@ class PunchesController < ApplicationController
 
   def destroy
   end
+  
+  def punch_time_now
+    Time.new
+  end
+  
+  def punch_params
+    
+  end
+  
 end
