83789ca95e460683199be771a9074deb57cabc72
Hironori <janhampino@gmail.com>
2018-10-19 23:58:41 +0000

    Finish make punch date update in edit

--- app/controllers/punches_controller.rb
+++ app/controllers/punches_controller.rb
@@ -15,18 +15,32 @@ class PunchesController < ApplicationController
       else
         log_in user
       end
-      if params[:punch][:status] == "in"
-        @punch = @user.punches.new(status: params[:punch][:status], punch_at_in: Time.new)
-        data = @punch.punch_at_in
-        @punch.punch_date = to_year_date(data).to_i
-      else
-        punches = Punch.where(user_id: params[:punch][:user_id])
-        @punch = punches.find_by(punch_date: today)
-        @punch.punch_at_out = Time.new
-      end
-      if @punch.save
-        flash.now[:success] = "打刻が完了しました"
-        redirect_to '/punches'
+      if params[:punch][:status] == "in"  #出社を選択してPunchした時
+        if already_punch_in?  #すでに出社時間を打刻済の時
+          flash[:error] = "本日の出社時間はすでに打刻済みです"
+          redirect_to '/punches'
+        else  #新しく打刻時間を作成する時
+          @punch = @user.punches.new(status: params[:punch][:status], punch_at_in: Time.new)
+          data = @punch.punch_at_in
+          @punch.punch_date = to_year_date(data).to_i #打刻年日のみを抽出してpunch_dateカラムへ
+          if @punch.save  #出社時間の打刻が保存された時
+            flash.now[:success] = "打刻が完了しました"
+            redirect_to '/punches'
+          end
+        end
+      else  #退社を選択してPunchした時
+        if already_punch_out?
+          flash[:error] = "本日の退社時間はすでに打刻済みです"
+          redirect_to '/punches'
+        else
+          punches = Punch.where(user_id: params[:punch][:user_id])
+          @punch = punches.find_by(punch_date: today) #ユーザー&出社時間が一致するデータを抽出
+          @punch.update_attributes(status: "out", punch_at_out: Time.new)
+          if @punch.save  #退社時間の打刻が保存された時
+            flash.now[:success] = "打刻が完了しました"
+            redirect_to '/punches'
+          end
+        end
       end
     else
       flash[:error] = "アカウントが未選択、またはパスワードが正しくありません"
@@ -36,18 +50,20 @@ class PunchesController < ApplicationController
 
   def edit
     @punch = Punch.find_by(id: params[:id])
-    if params[:edit] == "in"  #出社時間の編集リンク
+    if params[:edit] == "in"  #編集リンク(出社時間)を押した時
       session[:punch_at] = "in"
-    else                      #退社時間の編集リンク 
+    else  #編集リンク(退社時間)を押した時
       session[:punch_at] = nil
     end
   end
 
   def update
     @punch = Punch.find_by(id: params[:id])
-    if session[:punch_at] == "in"
+    if session[:punch_at] == "in" #編集更新リンク(出社時間)を押した時
       @punch.punch_at_in = Time.zone.local(params[:punch]["punch_at_in(1i)"].to_i, params[:punch]["punch_at_in(2i)"].to_i, params[:punch]["punch_at_in(3i)"].to_i, params[:punch]["punch_at_in(4i)"].to_i, params[:punch]["punch_at_in(5i)"].to_i)
-    else
+      in_date = Time.zone.local(params[:punch]["punch_at_in(1i)"].to_i, params[:punch]["punch_at_in(2i)"].to_i, params[:punch]["punch_at_in(3i)"].to_i)
+      @punch.punch_date = to_year_date(in_date).to_i
+    else  #編集更新リンク(退社時間)を押した時
       @punch.punch_at_out = Time.zone.local(params[:punch]["punch_at_out(1i)"].to_i, params[:punch]["punch_at_out(2i)"].to_i, params[:punch]["punch_at_out(3i)"].to_i, params[:punch]["punch_at_out(4i)"].to_i, params[:punch]["punch_at_out(5i)"].to_i)
     end
     if @punch.save
@@ -73,6 +89,17 @@ class PunchesController < ApplicationController
   def today
     now = Time.new
     return to_year_date(now).to_i
+  end
+  
+  #今日の打刻がすでに存在するかどうかを判別する
+  def already_punch_in?
+    punch = Punch.find_by(user_id: params[:punch][:user_id], punch_date: today)
+    !punch.nil?
+  end
+  
+  def already_punch_out?
+    punch = Punch.find_by(user_id: params[:punch][:user_id], punch_date: today)
+    !punch.punch_at_out.nil?
   end
   
   #beforeアクション
